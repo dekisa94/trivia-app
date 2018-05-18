fb33e7af422f8752cdd19afc46c99f603d5278fb
dekisa94 <dsimic94@gmail.com>
2018-05-18 18:44:35 +0200

    Task1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <router-link class="navbar-brand" to="/">Trivia-app</router-link>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">Page2</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TriviaApp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1>TriviaApp</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TriviaApp',
+}
+</script>
